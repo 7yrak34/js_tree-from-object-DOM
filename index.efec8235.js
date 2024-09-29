2f581c2320488d955f787835f7f70b2abfc958ea
Zyrak Nur <noorzirak7@gmail.com>
2024-09-29 22:59:40 +0100

    make build

---
+++ index.efec8235.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=Object.keys(t);if(r.length){var a=document.createElement("ul");r.forEach(function(n){var r=document.createElement("li");r.append(n),Object.keys(t[n]).length&&e(r,t[n]),a.append(r)}),n.append(a)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.efec8235.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const keys = Object.keys(data);
+    if (!keys.length) return;
+    const list = document.createElement("ul");
+    keys.forEach((key)=>{
+        const item = document.createElement("li");
+        item.append(key);
+        if (Object.keys(data[key]).length) createTree(item, data[key]);
+        list.append(item);
+    });
+    element.append(list);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
